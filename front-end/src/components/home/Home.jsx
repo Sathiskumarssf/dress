57d51d25edea75e7d89b3c033755532da605b7f7
Educative Learner <sathiskumarsk2001@gmail.com>
2024-04-10 23:45:53 +0530

    add search bar to home

--- front-end/src/components/home/Home.jsx
+++ front-end/src/components/home/Home.jsx
@@ -1,73 +1,82 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import {Link}  from 'react-router-dom';
-import { useLocation  } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import Navbar from '../../parts/Navbar';
-import './home.css'
+import './home.css';
 
 const Home = () => {
-
   const location = useLocation();
   const useremail = new URLSearchParams(location.search).get('email');
 
-     const [message, setMessage] = useState('');
-    const [products,setprodusts]=useState([])
-   
-    useEffect(()=>{
-        fetchData();
-    })
+  const [message, setMessage] = useState('');
+  const [products, setProducts] = useState([]);
+  const [query, setQuery] = useState('');
 
-    async function storetocheckout( path,name,prize,gender,code,useremail){
-      try{
-          const response = await axios.post('http://localhost:5000/checkout', {  path,name,prize,gender,code,useremail});
-          setMessage(response.data.message);
-          alert(message )
-           
-          
+  const handleChange = (e) => {
+    setQuery(e.target.value);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, [query]);
+
+  const fetchData = async () => {
+    try {
+      const result = await axios.get("http://localhost:5000/products", {
+        params: { query }
+      });
+      console.log(result.data);
+      setProducts(result.data);
+    } catch (err) {
+      console.log("something wrong");
+    }
+  };
+
+  const storeToCheckout = async (path, name, prize, gender, code, useremail) => {
+    try {
+      const response = await axios.post('http://localhost:5000/checkout', { path, name, prize, gender, code, useremail });
+      setMessage(response.data.message);
+      alert(message);
     } catch (error) {
-           setMessage(error.response.data.error);
-       
+      setMessage(error.response.data.error);
     }
-    }
+  };
 
-    const fetchData = async ()=>{
-        try {
-            const result = await axios("http://localhost:5000/products");
-             console.log(result.data);
-            setprodusts(result.data)
-        } catch (err) {
-            console.log("something wrong");
-        }
-    }
   return (
-    <div> 
-        <Navbar/>
-         {/* <button className='checkout-btn btn btn-success' >Go To Checkout</button>   */}
-         <Link to={`/checkout?useremail=${useremail}`}>
-            <button type="button" class="checkout-btn btn btn-success">Go To Checkout</button>
-          
-          </Link>
-        <div className='text-center'>
+    <div>
+      <Navbar />
+      <Link to={`/checkout?useremail=${useremail}`}>
+        <button type="button" className="checkout-btn btn btn-success">Go To Checkout</button>
+      </Link>
+      <div className='text-center'>
         <div className='text-center product-container-all'>
-        <h2 className='primary'>You are welcome to our shop</h2>
-      <ul className='product-container   w-100' style={{textDecoration:'none'}}>
-        {products.map(product => (
-          <li className='product-container-iterms  flex-sm-column  pt-1 m-4' style={{textDecoration:'none'}}   key={product.id}>
-             <img src={product.img_path} alt={product.name} className='product-image' />
-
-            
-            <h1  >{product.name}</h1>
-            <div  >{product.prize}</div>
-            <div  >{product.gender}</div>
-            <button className='btn btn-primary' onClick={() => storetocheckout(product.img_path, product.name, product.prize, product.gender, product.itermscode,useremail)}>Add to checkout</button>
-          </li>
-        ))}
-      </ul>
-    </div>
-   
+          <div className='searchbar-heading'>
+            <h2 className='primary'>You are welcome to our shop</h2>
+            <input
+              className="me-2"
+              type="search"
+              placeholder="Search"
+              aria-label="Search"
+              value={query}
+              onChange={handleChange}
+            />
+            <button className="btn btn-success" type="submit">Search</button>
+          </div>
+          <ul className='product-container w-100' style={{ textDecoration: 'none' }}>
+            {products.map(product => (
+              <li className='product-container-items flex-sm-column pt-1 m-4' style={{ textDecoration: 'none' }} key={product.id}>
+                <img src={product.img_path} alt={product.name} className='product-image' />
+                <h1>{product.name}</h1>
+                <div>{product.prize}</div>
+                <div>{product.gender}</div>
+                <button className='btn btn-primary' onClick={() => storeToCheckout(product.img_path, product.name, product.prize, product.gender, product.itermscode, useremail)}>Add to checkout</button>
+              </li>
+            ))}
+          </ul>
         </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
